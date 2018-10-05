0a0797e0a100f37b24f44c406885f5bd7e3f60d4
tobalo <nexxis817@gmail.com>
2018-10-05 13:23:26 -0500

    changed response

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
+    res.send({ send: 'yeet'});
 });
 
 const PORT = process.env.PORT || 5000;
