3b2152fa73a9b465113d7ee0acf73eb882524074
tobalo <nexxis817@gmail.com>
2018-10-05 14:27:13 -0500

    Added google oAuth modules

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
-// Initialize library
+// Initialize libraries
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ send: 'yeet'});
-});
+var clientID = null;
+var clientSecret = null;
+
+passport.use(new GoogleStrategy());
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
