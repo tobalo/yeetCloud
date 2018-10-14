293e155b93959848a32087b93af0d148d75bb6f4
tobalo <nexxis817@gmail.com>
2018-10-14 17:15:28 -0500

    added Google OAuth route handler

--- server/index.js
+++ server/index.js
@@ -6,9 +6,23 @@ const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        (accessToken) => {
+            console.log(accessToken);
+        }
+    )   
+);
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google',{
+    scope: ['profile', 'email']
     })
 );
 
