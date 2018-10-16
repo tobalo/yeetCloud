7e000e6401c5acb739304bb1bb1702b011ebb8b2
tobalo <nexxis817@gmail.com>
2018-10-16 13:12:31 -0500

    refactor

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 // Initialize libraries
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
 require('./services/passport');
 
 
 const app = express();
 
-require('./routes/authRoutes')(app);
+// pass app value to route handler
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,7 @@
 const passport = require('passport');
 
-
+// API to google OAuth route handler
 module.exports = app => {
-
     app.get(
         '/auth/google', 
         passport.authenticate('google', {
