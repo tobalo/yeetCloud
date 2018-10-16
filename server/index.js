a503187826a5d5cab86f56cb7875bab90e25093c
tobalo <nexxis817@gmail.com>
2018-10-16 12:37:31 -0500

    Refactored routes and authentication

--- server/index.js
+++ server/index.js
@@ -1,30 +1,11 @@
 // Initialize libraries
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+
 
 const app = express();
 
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.clientID,
-            clientSecret: keys.ClientSecret,
-            callbackURL: '/auth/google/callback'
-        }, 
-        (accessToken) => {
-            console.log(accessToken);
-        }
-    )   
-);
-
-app.get(
-    '/auth/google', 
-    passport.authenticate('google',{
-    scope: ['profile', 'email']
-    })
-);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
