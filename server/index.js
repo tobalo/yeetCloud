3cf49361b279115072f6530e8040c36f4b0c77c6
tobalo <nexxis817@gmail.com>
2018-10-17 09:50:03 -0500

    Added user schema

--- server/index.js
+++ server/index.js
@@ -2,16 +2,26 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const mongoose = require('mongoose');
+require('./models/user');
 require('./services/passport');
 const keys = require('./config/keys');
+
 
 
 const app = express();
 
 // connect to DB
 mongoose.connect(keys.mongoURI, {
-    useNewUrlParser: true
-});
+    useNewUrlParser: true},
+     function(err){
+        if(!err) {
+            console.log("Database is connected ... nn");
+        } else {
+            console.log("Error connecting database ... nn");
+        }
+
+    }
+);
 
 // pass app value to authroute handler
 authRoutes(app);
