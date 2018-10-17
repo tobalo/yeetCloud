a0df6cc74aee5bfee7e663abf135ebf963b05a78
tobalo <nexxis817@gmail.com>
2018-10-17 12:26:10 -0500

    Added user schema

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 
 passport.use(
@@ -11,9 +14,11 @@ passport.use(
             callbackURL: '/auth/google/callback'
         }, 
         (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile', profile);
+            new User({ 
+                googleID: profile.id,
+                name: profile.name,
+                email: profile.email
+            }).save();
         }
     )   
 );
