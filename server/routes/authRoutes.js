ac23aff553fee468f8e4c7d79345841fbbb5a720
tobalo <nexxis817@gmail.com>
2018-10-19 10:28:39 -0500

    Added logout functionality

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,11 +5,17 @@ module.exports = app => {
     app.get(
         '/auth/google', 
         passport.authenticate('google', {
-        scope: ['profile']
+        scope: ['profile', 'email']
         })
     );
 
     app.get('/auth/google/callback', passport.authenticate('google'));
+    
+    app.get('/auth/user/logout', (req, res) => {
+        console.log('User:', req.user, 'logged out');
+        req.logout();
+        res.send(req.session);
+    });
 
     app.get('/auth/user', (req, res) => {
         res.send(req.user);
