55769decfe246fb551ad3c64d22740584f3a2718
tobalo <nexxis817@gmail.com>
2018-10-23 13:52:44 -0500

    added authReducer logic

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/App';
 
-const store = createStore(() => [], {}, applyMiddleware());
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(() => reducers, {}, applyMiddleware());
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector('#root'));
