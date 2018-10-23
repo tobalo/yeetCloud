4283ad25560b33aee102a09f43edf16a42fdd4b0
tobalo <nexxis817@gmail.com>
2018-10-23 14:38:07 -0500

    Added header component

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>;
+const AccountNew = () => <h2>AccountNew</h2>;
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return (
         <div>
-            Yeet
+            <BrowserRouter>
+                <div>
+                    <Route path="/" component={Header} />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/accounts" component={Dashboard} />
+                    <Route path="/accounts/new" component={AccountNew} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
