f4bd21fc606d6f9b43363ed66da0270d9d967e26
tobalo <nexxis817@gmail.com>
2018-10-24 17:00:38 -0500

    added feature to recognize if user is logged in

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,18 +1,24 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
     render(){
-        return (
-          <nav>
-            <div className="nav-wrapper grey darken-4">
-              <a href="/" className="left brand-logo"> Yeet</a>
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="/auth/google">Login With Google</a></li>
-              </ul>
-            </div>
-          </nav>
-        );
+      console.log(this.props);
+      return (
+        <nav>
+          <div className="nav-wrapper grey darken-4">
+            <a href="/" className="left brand-logo"> Yeet</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><a href="/auth/google">Login With Google</a></li>
+            </ul>
+          </div>
+        </nav>
+      );
     }
 }
 
-export default Header;
+function mapStateToProps(state){
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header);
