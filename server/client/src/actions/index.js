27530423573312dfafe5b25bc569dec9cd7f2ff3
tobalo <nexxis817@gmail.com>
2018-10-24 11:57:07 -0500

    Refactor

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-async const fetchUser = () => {
-    await axios.get('/auth/user');
+const fetchUser = () => {
+    return function(dispatch){
+        axios.get('/auth/user')
+        .then(res => dispatch({
+            type: FETCH_USER,
+            payload: res 
+        }));
+    }
 };
